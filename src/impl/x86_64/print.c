53832650018d9e57fd99afeb83ee0cb016ec2fb6
Colleen Lohr <cmlohr@outlook.com>
2022-01-09 18:04:15 -0600

    fixed typo

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -1,47 +1,54 @@
 #include "print.h"
 
-
 const static size_t NUM_COLS = 80;
 const static size_t NUM_ROWS = 25;
 
-struct Char {
+struct Char
+{
     uint8_t character;
     uint8_t color;
 };
 
-struct Char* buffer = (struct Char*) 0xB8000;
+struct Char *buffer = (struct Char *)0xb8000;
 size_t col = 0;
 size_t row = 0;
-uint8_t color = PRINT_COLOR_BLACK | PRINT_COLOR_LIGHT_GREEN << 4;
+uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
 
-
-void clear_row(size_t row) {
-    struct Char empty = (struct Char) {
-        character: ' ',
-        color: color,
+void clear_row(size_t row)
+{
+    struct Char empty = (struct Char){
+        character : ' ',
+        color : color,
     };
-    for(size_t col = 0; col < NUM_COLS; col++) {
-        // custom code below
-        buffer[row * NUM_COLS + col] = empty;
+
+    for (size_t col = 0; col < NUM_COLS; col++)
+    {
+        buffer[col + NUM_COLS * row] = empty;
     }
 }
 
-void print_clear() {
-    for (size_t i = 0; i < NUM_ROWS * NUM_COLS; i++) {
+void print_clear()
+{
+    for (size_t i = 0; i < NUM_ROWS; i++)
+    {
         clear_row(i);
     }
 }
 
-void print_newline() {
+void print_newline()
+{
     col = 0;
 
-    if(row < NUM_ROWS -1) {
+    if (row < NUM_ROWS - 1)
+    {
         row++;
         return;
     }
 
-    for (size_t row = 1; row < NUM_ROWS; row++) {
-        for (size_t col = 0; col < NUM_COLS; col++) {
+    for (size_t row = 1; row < NUM_ROWS; row++)
+    {
+        for (size_t col = 0; col < NUM_COLS; col++)
+        {
             struct Char character = buffer[col + NUM_COLS * row];
             buffer[col + NUM_COLS * (row - 1)] = character;
         }
@@ -50,26 +57,35 @@ void print_newline() {
     clear_row(NUM_COLS - 1);
 }
 
-void print_char(char character) {
-    if (character == '\n') {
+void print_char(char character)
+{
+    if (character == '\n')
+    {
         print_newline();
         return;
     }
-        if (col > NUM_COLS) {
-            print_newline();
-        }
-        buffer[col + NUM_COLS * row] = (struct Char) {
-            character: (uint8_t) character,
-            color: color,
-        };
-        col++;
+
+    if (col > NUM_COLS)
+    {
+        print_newline();
     }
 
-void print_string(char* string) {
-    for (size_t i = 0; 1; i++) {
-        char character = (uint8_t) str[i];
+    buffer[col + NUM_COLS * row] = (struct Char){
+        character : (uint8_t)character,
+        color : color,
+    };
 
-        if (character == '\0') {
+    col++;
+}
+
+void print_str(char *str)
+{
+    for (size_t i = 0; 1; i++)
+    {
+        char character = (uint8_t)str[i];
+
+        if (character == '\0')
+        {
             return;
         }
 
@@ -77,6 +93,7 @@ void print_string(char* string) {
     }
 }
 
-void print_set_color(uint8_t foreground, uint8_t background) {
+void print_set_color(uint8_t foreground, uint8_t background)
+{
     color = foreground + (background << 4);
 }

e986f98db48f5f088714f85bcc0b053628f5d677
Colleen Lohr <cmlohr@outlook.com>
2022-01-09 18:29:58 -0600

    finalized print statement

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,6 +3,6 @@
 void kernel_main()
 {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Colleen's 64-bit kernel!");
+    print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
+    print_str("64-bit kernel by Colleen Lohr 2022");
 }

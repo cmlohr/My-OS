6349db80bf3a5f0e0860982a8bbadfe8d68ea7ca
Colleen Lohr <cmlohr@outlook.com>
2022-01-09 18:03:29 -0600

    updated for printing colors to screen

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,5 +1,7 @@
+#include "print.h"
+
 void kernel_main() {
-    print_clear()
-    print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK)
-    print_str("Welcome to Colleen's kernel!\n")
+    print_clear();
+    print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
+    print_str("Welcome to Colleen's kernel!\n");
 }
